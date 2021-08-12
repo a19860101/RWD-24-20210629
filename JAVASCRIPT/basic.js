fe7a3e4d2e0fca8bc5980c856c3f7017d7d113bd
Zac <a19860101@gmail.com>
2021-08-12 21:20:20 +0800

    20210812

--- JAVASCRIPT/basic.js
+++ JAVASCRIPT/basic.js
@@ -1,1 +1,38 @@
-alert('hello');
+// 變數 variable
+
+/* 
+變數命名規則
+1. 大小寫有別
+2. 開頭不可使用數字
+3. 符號只可使用底線(_)與錢字號($)
+*/
+var a = 'hello';
+var a = 100;
+console.log(a);
+
+let b = 'hello b';
+console.log(b);
+/* 
+    var vs let,const
+    1. let const 不可重複宣告
+    2. var是function scope , let const是block scope
+*/
+
+// 常數 constant variable
+// 不可重新賦值
+const c = null;
+console.log(c);
+
+/* 
+    資料型態
+    1. 數值 number
+    2. 字串 string
+    3. 布林 boolean
+    4. 未定義 undefined
+    5. 空值 null
+
+    6. 陣列 Array
+    7. 物件 Object
+    8. 函式 Function
+
+*/
---
+++ JAVASCRIPT/operator.js
@@ -0,0 +1,25 @@
+// 運算子
+
+let x = 10;
+let y = 6;
+
+let a = 10;
+let b = '10';
+
+// 算術運算子
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);//求餘數
+
+// 比較運算子
+console.log(x > y);
+console.log(x < y);
+console.log(x >= y);
+console.log(x <= y);
+console.log(x == y);
+console.log(x === y);// 值與型態皆須相同
+console.log(a == b);
+console.log(a === b);// 值與型態皆須相同
+console.log(x != y);
