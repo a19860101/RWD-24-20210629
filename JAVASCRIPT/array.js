e98664dfc917db9a29ed7ca09654e4f4d9b82197
Zac <a19860101@gmail.com>
2021-08-17 19:45:45 +0800

    20210817

--- JAVASCRIPT/array.js
+++ JAVASCRIPT/array.js
@@ -5,8 +5,26 @@
 
 // let foods = new Array('滷肉飯','牛肉麵','肉圓');
 
-let foods = ['滷肉飯','牛肉麵','肉圓'];
-console.log(foods);
-console.log(foods[0]);
-console.log(foods[1]);
-console.log(foods[2]);
+let foods = ['滷肉飯','牛肉麵','肉圓','臭豆腐','豆花'];
+// console.log(foods);
+// console.log(foods[0]);
+// console.log(foods[1]);
+// console.log(foods[2]);
+
+// length
+console.log(foods.length)
+
+// 陣列迭代
+// for(let i=0;i<foods.length;i++){
+//     console.log(foods[i]);
+// }
+
+// for...of
+for(let food of foods){
+    console.log(food);
+}
+
+// for...in
+for(let i in foods){
+    console.log(foods[i]);
+}
