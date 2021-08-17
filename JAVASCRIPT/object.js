92cacdeaada85ceac7bcc37d31f6c75c82bef46a
Zac <a19860101@gmail.com>
2021-08-17 21:14:22 +0800

    20210817

---
+++ JAVASCRIPT/object.js
@@ -0,0 +1,18 @@
+// let drinks = new Object();
+// drinks.name = '珍珠奶茶';
+// drinks.price = 50;
+// drinks.size = '750ml';
+
+let drinks = {
+    name: '珍珠奶茶',
+    price: 50,
+    size: '750ml'
+}
+
+console.log(drinks);
+console.log(drinks.name);
+console.log(drinks.price);
+console.log(drinks.size);
+console.log(drinks['name']);
+console.log(drinks['price']);
+console.log(drinks['size']);
