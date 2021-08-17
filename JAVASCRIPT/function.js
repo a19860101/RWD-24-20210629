5579ad1480f969af9b537eb219660f0814a74b5e
Zac <a19860101@gmail.com>
2021-08-17 21:48:56 +0800

    20210817

--- JAVASCRIPT/function.js
+++ JAVASCRIPT/function.js
@@ -1,16 +1,32 @@
 //函式陳述式 (提升性)
-console.log(test());
+// console.log(test());
 
-function test(){
-    return 'test';
-    // alert('test');
-}
+// function test(){
+//     return 'test';
+// }
 
 
 
 //函式表達式
-let test2 = function(){
-    return 'test2';
+// let test2 = function(){
+//     return 'test2';
+// }
+
+// console.log(test2());
+
+function hello(name){
+    return `hello ${name}`;
 }
 
-console.log(test2());
+console.log(hello('John'));
+
+function square(x){
+    return x*x;
+}
+console.log(square(11));
+
+function dollar(nt,exchange){
+    let tax = 1.8;
+    return nt * exchange * tax;
+}
+console.log(dollar(100,28));
